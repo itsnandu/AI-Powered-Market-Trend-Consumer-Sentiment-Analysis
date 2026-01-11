cfe4ec9ba4ad17f939aa24756e7758f31919f0a1
Nandini Choudhary <130913480+itsnandu@users.noreply.github.com>
2026-01-11 22:58:48 +0530

    Second Commit

--- dashboard.py
+++ dashboard.py
@@ -19,7 +19,7 @@ st.markdown("Consumer sentiment, topic trend, and social insights from reviews, 
 @st.cache_data
 def load_data():
     reviews = pd.read_csv("data/category_wise_lda_output_with_topic_labels.csv")
-    
+
     reddit = pd.read_excel("data/reddit_category_trend_data.xlsx")
     
     news = pd.read_csv("data/news_data_with_sentiment.csv")
@@ -140,3 +140,168 @@ fig_trend = px.line(
 )
 
 st.plotly_chart(fig_trend, use_container_width=True)
+
+
+# category trend over time 
+st.subheader("Category Trend Over Time (Product Demand)")
+
+category_trend=(
+    filtered_reviews.groupby([pd.Grouper(key="review_date", freq="M"), "category"])
+    .size()
+    .reset_index(name="count")
+)
+
+fig_category_trend = px.line(
+    category_trend,
+    x="review_date",
+    y="count",
+    color="category",
+    title="Monthly Category Demand Trend"
+)
+
+st.plotly_chart(fig_category_trend, use_container_width=True)
+
+
+# category vs sentiment 
+# Category-wise Sentiment Distribution
+
+st.subheader("Category-wise Sentiment Distribution")
+
+cat_sent=(
+    filtered_reviews
+    .groupby(["category", "sentiment_label"])
+    .size()
+    .reset_index(name="count")
+)
+
+fig_cat = px.bar(
+    cat_sent,
+    x="category",
+    y="count",
+    color="sentiment_label",
+    barmode="group"
+)
+
+st.plotly_chart(fig_cat, use_container_width=True)
+
+
+# topic distribution
+
+
+st.subheader("Topic Insights")
+
+topic_dist = (
+    filtered_reviews["topic_label"]
+    .value_counts()
+    .reset_index()
+)
+
+topic_dist.columns=["Topic", "Count"]
+
+fig_topic = px.bar(
+    topic_dist, 
+    x="Topic",
+    y="Count",
+    title="Topic Distribution"
+)
+
+st.plotly_chart(fig_topic, use_container_width=True)
+
+
+# Reddit category trend 
+
+st.subheader("Reddit Category Popularity")
+
+reddit_trend=(
+    reddit_df
+    .groupby("category_label")
+    .size()
+    .reset_index(name="mentions")
+    .sort_values("mentions", ascending=False)
+)
+
+fig_reddit = px.bar(
+    reddit_trend, 
+    x="category_label",
+    y="mentions",
+    title="Trending categories on Reddit"
+)
+
+st.plotly_chart(fig_reddit, use_container_width=True)
+
+# news sentiment 
+
+st.subheader("News Sentiment Overview")
+
+news_sent=(
+    news_df
+    .groupby("sentiment_label")
+    .size()
+    .reset_index(name="count")
+)
+
+fig_news=px.pie(
+    news_sent,
+    names="sentiment_label",
+    values="count",
+    title="News Sentiment distribution"
+)
+
+st.plotly_chart(fig_news, use_container_width=True)
+
+
+# News Category Distribution
+# home work
+
+
+
+# trending on all platforms
+# Home work: how we can make it better
+st.subheader("Cross-Source Category Camparision")
+# review category count
+
+review_cat=(
+    reviews_df
+    .groupby("category")
+    .size()
+    .reset_index(name="Review Mentions")
+)
+
+# reddit category count
+
+reddit_cat=(
+    reddit_df
+    .groupby("category_label")
+    .size()
+    .reset_index(name="Reddit Mentions")
+    .rename(columns={"category_label":"category"})
+)
+
+# news category count
+
+news_cat=(
+    news_df
+    .groupby("category")
+    .size()
+    .reset_index(name="News Mentions")
+)
+
+
+# merge all 
+
+category_compare = review_cat\
+    .merge(reddit_cat, on="category", how="outer")\
+    .merge(news_cat, on="category", how="outer")\
+    .fillna(0)
+    
+fig_compare = px.bar(
+    category_compare, 
+    x="category",
+    y=["Review Mentions", "Reddit Mentions", "News Mentions"],
+    title="Category Presence Across Reviews, Reddit and News",
+    barmode="group"
+)
+
+st.plotly_chart(fig_compare, use_container_width=True)
+
+st.dataframe(category_compare)
